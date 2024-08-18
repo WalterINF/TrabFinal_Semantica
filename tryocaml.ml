09c75b96a3da975184e993e4709c87cd0ca4c5a1
Walter Frank <112022468+WalterINF@users.noreply.github.com>
2024-08-17 21:23:09 -0300

    Update tryocaml.ml

--- tryocaml.ml
+++ tryocaml.ml
@@ -347,13 +347,13 @@ let rec eval (renv:renv) (e:expr) :valor =
       let v1 = eval renv e1 in
       let v2 = eval renv e2 in
       (match v2 with
-         VClos(x,ebdy,renv') ->
-           let renv'' = update renv' x v1
-           in eval renv'' ebdy 
-       | VRClos(f,x,ebdy,renv') ->
-           let renv''  = update renv' x v2 in
-           let renv''' = update renv'' f v1
-           in eval renv''' ebdy
+         VClos(x,eb,renv1) ->
+           let renv2 = update renv1 x v1
+           in eval renv2 eb 
+       | VRClos(f,x,eb,renv1) ->
+           let renv2  = update renv1 x v2 in
+           let renv3 = update renv2 f v1
+           in eval renv3 eb
        | _ -> raise BugTypeInfer)
       
       
@@ -408,12 +408,4 @@ int_bse(Nothing(TyBool))
 int_bse(MatchWithNothing(Nothing(TyBool),Num 5,"ddd",Num 6))
 int_bse(If(Binop(Eq,Num 5,Num 5),Num 20,Num 30))
   
-*)
-
-
-
-
-
-                        
-  
-                        
+*)              
